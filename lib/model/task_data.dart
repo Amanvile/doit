0a1551d21a2ce5694b75a0c5a1d1a0fc0af14207
amanvile <deribaman@gmail.com>
2025-11-25 22:56:56 +0300

    using provider package state management task screen decreased prop drilling

---
+++ lib/model/task_data.dart
@@ -0,0 +1,8 @@
+import 'package:provider/provider.dart';
+import 'task.dart';
+class TaskData extends ChangeNotifierProvider{
+  // late List <Task> tasks;
+  TaskData({super.key, required super.create});
+
+
+}
